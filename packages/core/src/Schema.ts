8b5a5c72376375d1939c676a62da31c19d367239
Brie <723547+brieb@users.noreply.github.com>
2021-03-18 20:26:16 -0700

    Replace __DEV__ with NODE_ENV check

--- packages/core/src/Schema.ts
+++ packages/core/src/Schema.ts
@@ -76,7 +76,7 @@ export default class Schema<T = unknown> {
     relation: string,
     polymorph?: PolymorphRelation,
   ): this {
-    if (__DEV__) {
+    if ("production" !== process.env.NODE_ENV) {
       if (!(schema instanceof Schema)) {
         throw new TypeError(`Relation "${attribute}" is not a valid schema.`);
       } else if (this.relationTypes[attribute] && this.relationTypes[attribute] !== relation) {
